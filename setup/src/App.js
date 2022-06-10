d52aca3ffa1f14530c7537112b8d304ba24a1f60
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-06-10 18:48:12 -0400

    Setup of fetch from an API

--- setup/src/App.js
+++ setup/src/App.js
@@ -4,8 +4,42 @@ import Tours from './Tours';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project';
+
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // fetching data
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = response.json();
+      console.log(tours);
+    } catch (error) {
+      console.log('There has been an error');
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // loading
+  if (loading) {
+    // if loading is set to true, then return a
+    // main div with the loading component
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
